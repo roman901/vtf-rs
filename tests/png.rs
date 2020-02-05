403c22b353dc7be7c99cd7a344861b28d81a5ba0
Robin Appelman <robin@icewind.nl>
2020-02-05 15:30:57 +0100

    add more decoder tests

--- tests/png.rs
+++ tests/png.rs
@@ -1,4 +1,4 @@
-use image::{open, GenericImageView, RgbaImage};
+use image::{open, DynamicImage, GenericImageView};
 use std::collections::hash_map::DefaultHasher;
 use std::fs::File;
 use std::hash::{Hash, Hasher};
@@ -6,8 +6,21 @@ use std::io::Read;
 use std::vec::Vec;
 
 #[test]
-fn test_to_dxt5() {
+fn test_to_png_dxt5() {
     test_image("tests/data/rust_dxt5.vtf", "tests/data/rust_dxt5.png");
+}
+
+#[test]
+fn test_to_png_rgb8() {
+    test_image("tests/data/rust_rgb_8.vtf", "tests/data/rust_rgb_8.png");
+}
+
+#[test]
+fn test_to_png_rgb8_alpha() {
+    test_image(
+        "tests/data/rust_rgb_8_alpha.vtf",
+        "tests/data/rust_rgb_8_alpha.png",
+    );
 }
 
 fn test_image(input: &str, expected: &str) {
@@ -22,14 +35,11 @@ fn test_image(input: &str, expected: &str) {
 
     assert_eq!(expected.dimensions(), image.dimensions());
 
-    assert_eq!(
-        hash(expected.as_rgba8().unwrap()),
-        hash(image.as_rgba8().unwrap())
-    );
+    assert_eq!(hash(expected), hash(image));
 }
 
-fn hash(image: &RgbaImage) -> u64 {
+fn hash(image: DynamicImage) -> u64 {
     let mut hasher = DefaultHasher::new();
-    image.hash(&mut hasher);
+    image.into_rgba().hash(&mut hasher);
     hasher.finish()
 }
